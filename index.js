0587b12c416d88dfdd47590ca44ae4dfcae5aa31
Roman Demikhov <demikhovroman@gmail.com>
2020-11-05 17:42:06 +0300

    done

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const myContacts = require('./contacts.js');
-const argv = require('yargs').argv;
+// const myContacts = require('./contacts.js');
+// const argv = require('yargs').argv;
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      myContacts.listContacts()
-      break;
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case 'list':
+//       myContacts.listContacts()
+//       break;
 
-    case 'get':
-      myContacts.getContactById(id)
-      break;
+//     case 'get':
+//       myContacts.getContactById(id)
+//       break;
 
-    case 'add':
-      myContacts.addContact(name, email, phone)
-      break;
+//     case 'add':
+//       myContacts.addContact(name, email, phone)
+//       break;
 
-    case 'remove':
-      myContacts.removeContact(id)
-      break;
+//     case 'remove':
+//       myContacts.removeContact(id)
+//       break;
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
-  }
-}
+//     default:
+//       console.warn('\x1B[31m Unknown action type!');
+//   }
+// }
 
-invokeAction(argv);
+// invokeAction(argv);
 
---
+++ src/contacts/contacts.model.js
@@ -0,0 +1,86 @@
+const uuid = require('uuid');
+const fs = require('fs');
+const fsp = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.join(__dirname, '../../db/contacts.json');
+
+async function listContacts() {
+  const contacts = await fsp.readFile(contactsPath, 'utf-8');
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contactsList = await fsp.readFile(contactsPath, 'utf-8');
+  const updateList = await JSON.parse(contactsList);
+  const searchContact = await updateList.find(
+    el => el.id === Number(contactId),
+  );
+  return searchContact;
+}
+
+async function addContact(contactParams) {
+  const contactsList = await fsp.readFile(contactsPath, 'utf-8');
+  const updateList = await JSON.parse(contactsList);
+  const newContact = {
+    ...contactParams,
+    id: uuid.v4(),
+  };
+    updateList.push(newContact);
+    await fsp.writeFile(contactsPath, JSON.stringify(updateList), err => {
+      if (err) {
+        throw err;
+      }
+    });
+  return updateList;
+}
+
+async function delContact(contactId) {
+  const contactsList = await fsp.readFile(contactsPath, 'utf-8');
+  const updateList = await JSON.parse(contactsList);
+  const findIndexOfEl = await updateList.findIndex(
+    el => el.id === Number(contactId),
+  );
+  if (findIndexOfEl === -1) {
+    return;
+  } else {
+    await updateList.splice(findIndexOfEl, 1);
+    await fsp.writeFile(contactsPath, JSON.stringify(updateList), err => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+}
+
+async function updateOneContact(contactId, contactParams) {
+    const contactsList = await fsp.readFile(contactsPath, 'utf-8');
+    const updateList = await JSON.parse(contactsList);
+    const contactIndex = updateList.findIndex(
+      el => el.id === Number(contactId),
+    );
+    if (contactIndex === -1) {
+      return;
+    } else {
+        updateList[contactIndex] = await {
+          ...updateList[contactIndex],
+          ...contactParams
+        };
+         await fsp.writeFile(contactsPath, JSON.stringify(updateList), err => {
+           if (err) {
+             throw err;
+           }
+         });
+        return updateList[contactIndex];
+    }
+}
+
+
+
+module.exports = {
+  listContacts,
+  addContact,
+  getContactById,
+  delContact,
+  updateOneContact
+};
